6a1d8d2678099b748d76578a755a9f8222a77430
zimoluo <zimo_luo@outlook.com>
2024-04-15 13:15:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Zimo Web Screenshot Tool
+
+This command-line tool allows users to take screenshots of Zimo Web pages with customized settings such as window dimensions, theme, and delays. It works as a handy script for me who need to take standardized screenshots for various purposes.
+
+## Features
+
+- Set browser window dimensions.
+- Apply custom themes.
+- Configure delays for screenshots to wait for dynamic content to load.
+- Save screenshots locally in PNG format.
+
+## Installation
+
+To run this tool, you'll need Python and Selenium. Here are the steps to set it up:
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/yourusername/web-screenshot.git
+   cd web-screenshot
+   ```
+2. **Install dependencies**:
+   You will need Python and pip installed. Then, you can install Selenium:
+   ```bash
+   pip install selenium
+   ```
+3. **Web Driver**:
+   You need to have at least one recognizable web driver installed on your device. This is typically bundled within a Chrome browser.
+
+## Usage
+
+To use the tool, navigate to the project directory and run the script with the required options. Example below:
+
+```bash
+python scraper.py --width 1200 --height 800 --theme dark --pathname about --delay 2
+```
+
+## Command Line Arguments
+
+- `-w`, `--width`: Width of the browser window (default: `1600`)
+- `-H`, `--height`: Height of the browser window (default: `900`)
+- `-theme`, `--theme`: Theme name to apply (default: `pixelland`)
+- `-pathname`, `--pathname`: Pathname of the URL (default: `blog`)
+- `-delay`, `--delay`: Delay in seconds before taking the screenshot (default: `0`)
+- `-f`, `--filename`: Output filename without the `.png` suffix (default: `webpage_screenshot`)
---
+++ screenshot.py
@@ -0,0 +1,62 @@
+import argparse
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+def take_screenshot(width, height, theme_name, pathname, delay, filename):
+    chrome_options = Options()
+    chrome_options.add_argument(f"--window-size={width},{height}")
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(f"https://www.zimoluo.me/{pathname}")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "menu-button"))).click()
+
+    time.sleep(0.4)
+
+    button_xpath = f"//button[.//img[@alt='Use {theme_name} theme']]"
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
+    time.sleep(0.4)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "menu-button"))).click()
+
+    time.sleep(1)
+
+    time.sleep(delay)
+
+    driver.save_screenshot(f'{filename}.png')
+    driver.quit()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Take a screenshot of a Zimo Web's page with specified settings.")
+    parser.add_argument('-w', '--width', type=int,
+                        default=1600, help='Width of the browser window')
+    parser.add_argument('-H', '--height', type=int,
+                        default=900, help='Height of the browser window')
+    parser.add_argument('-t', '--theme', type=str,
+                        default="pixelland", help='Theme name to apply')
+    parser.add_argument('-p', '--pathname', type=str,
+                        default="design", help='Path within Zimo Web')
+    parser.add_argument('-d', '--delay', type=float,
+                        default=0, help='Delay before taking the screenshot')
+    parser.add_argument('-f', '--filename', type=str,
+                        default='webpage_screenshot', help='Output filename without suffix')
+
+    args = parser.parse_args()
+
+    take_screenshot(args.width, args.height, args.theme,
+                    args.pathname, args.delay, args.filename)
+
+
+if __name__ == "__main__":
+    main()
