f4b12914de9c564254c0ae314c1b6d96c150d257
zimoluo <zimo_luo@outlook.com>
2025-02-21 22:28:40 -0500

    0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zimo-web-screenshot',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
